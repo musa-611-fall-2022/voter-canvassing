5513b47d8220a6936e82bb4bf506c5d9723600ba
Myron Bañez <banez@upenn.edu>
2022-10-30 19:48:46 -0400

    attempt at search box and removing former search filter

--- site/js/search-box.js
+++ site/js/search-box.js
@@ -1,27 +1,31 @@
-// // add event listener to search box
-// import voters from '../data/list-loader.js';
+// // // add event listener to search box
+// // import data from '../data/list-loader.js';
 // import { showVotersInList } from './voter-list';
 // import { voterList } from './voter-list';
 
 // let voterNameFilter = document.querySelector('#search-box-input');
+// let filteredVoter = data
+// // // function onSearchBoxInput() {
+// // //   showVotersInList();
+// // // }
 
-// // function onSearchBoxInput() {
-// //   showVotersInList();
+// // function onSearchBoxInput(data) {
+// //   // showVotersInList();
+// //   let filteredVoter = data;
+// //   const text = voterNameFilter.value;
+// //   filteredVoter = filteredVoter.filter(function(voter) {
+// //         const name = voter['First Name'].toLowerCase();
+// //         const hasText = name.includes(text);
+// //         return hasText;
+// //       });
+// //       return filteredVoter;
 // // }
 
 // function onSearchBoxInput() {
-//   // showVotersInList();
-//   let filteredVoter = voters;
-//   const text = voterNameFilter.value;
-//   filteredVoter = filteredVoter.filter(function(voter) {
-//         const name = voter['First Name'].toLowerCase();
-//         const hasText = name.includes(text);
-//         return hasText;
-//       });
-//       return filteredVoter;
+//   showVotersInList(filteredVoter, voterList);
 // }
 
-// voterNameFilter.addEventListener('input', () => {
-//   const filteredVoter = onSearchBoxInput();
-//   showVotersInList(filteredVoter, voterList);
-// });
+// // voterNameFilter.addEventListener('input', () => {
+// //   const filteredVoter = onSearchBoxInput();
+// //   showVotersInList(filteredVoter, voterList);
+// // });
