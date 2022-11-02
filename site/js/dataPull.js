6f220ef77367462f48d5a0ada920c65ce30617f1
henryfeinstein <98499896+henryfeinstein@users.noreply.github.com>
2022-11-02 10:14:40 -0400

    update function structure

--- site/js/dataPull.js
+++ site/js/dataPull.js
@@ -1,13 +1,22 @@
-function getVoterList(listNum) {
+function populateVoterList(listNum, map) {
     return fetch('data/voters_lists/' + listNum + '.csv')
     .then(resp => resp.text())
     .then(text => {
         const data = Papa.parse(text, { header: true });
         console.log(data['data']);
-        return data['data'];
+        populateVoterMap(data, map);
+        populateVoterMenu(data);
     });
 }
 
+function populateVoterMap(data, map) {
+    //pass
+}
+
+function populateVoterMenu(data) {
+    //pass
+}
+
 export {
-    getVoterList,
+    populateVoterList,
 };
--- site/js/main.js
+++ site/js/main.js
@@ -1,9 +1,8 @@
 import { initializeMap } from './map.js';
-import { getVoterList } from './dataPull.js';
+import { populateVoterList } from './dataPull.js';
 
 let map = initializeMap();
 
 let listNum = "0101";
-let list = getVoterList(listNum);
-console.log(list);
+populateVoterList(listNum, map);
 
