06680befb7a4e056efe2601d69e79cb4dadc0a72
Yesao <673392438@qq.com>
2022-11-02 15:10:14 -0400

    Create map.js to organize the map functions

--- site/script/main.js
+++ site/script/main.js
@@ -1,19 +1,10 @@
+import {initMap} from "./map.js";
+
 let app = {
 
 };
 
-function initMap() {
-    const map = L.map('map', {maxZoom: 22, preferCanvas: true }).setView([39.995, -75.13], 12);
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/keelbn/cl8w1pun9001514odcvwo00gb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2VlbGJuIiwiYSI6ImNqaWVseGZjZzA3emMzdnAxM296OTFjNG8ifQ.W2j9Y2mz4t6vGRyKJk_Nyw', {
-        maxZoom: 22,
-        minZoom: 10,
-        attribution: '© OpenStreetMap',
-    }).addTo(map);
-
-    return map;
-}
-
 const map = initMap();
 
 window.app = app;
+window.map = map;
