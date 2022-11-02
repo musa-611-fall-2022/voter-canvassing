0f25ef97f70657cdf568e1cfec7741b933dcde05
Ben Keel <bennkeel@gmail.com>
2022-11-02 12:51:57 -0400

    Slight update to html for basic elements and ID's

--- site/script/main.js
+++ site/script/main.js
@@ -1,6 +1,5 @@
 let app = {
-    currentTree: null,
-    notes: JSON.parse(localStorage.getItem('notes') || '{}')
+
 };
 
 function initMap() {
@@ -12,48 +11,9 @@ function initMap() {
         attribution: '© OpenStreetMap',
     }).addTo(map);
 
-    map.treeLayer = L.geoJSON(null, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng)
-    }).addTo(map);
-
-    map.treeLayer.addEventListener('click', onTreeClicked);
-
-    map.positionLayer = L.geoJSON(null).addTo(map);
-
     return map;
 }
 
-function downloadInventory() {
-    fetch('data/PPR_Tree_Inventory_2021.geojson')
-    .then(resp => resp.json())
-    .then(data => {
-        map.treeLayer.addData(data)
-    });
-}
-
-function userPositionSuccess(pos){
-    console.log(pos);
-    map.positionLayer.addData({
-        'type': 'Point', 
-        'coordinates': [pos.coords.longitude, pos.coords.latitude]
-    });
-    map.setView([pos.coords.latitude, pos.coords.longitude], 19); 
-}
-
-function userPositionFailure(err){
-    console.log(err);
-}
-
-function setupGeolocationEvent() {
-    navigator.geolocation.getCurrentPosition(
-        userPositionSuccess,
-        userPositionFailure,
-    )
-}
-
 const map = initMap();
-downloadInventory();
-setupGeolocationEvent();
-setupTreeInfoForm();
 
 window.app = app;
