0d123cf41d822a7b64fb7a28165112e4d6d338da
Myron Bañez <banez@upenn.edu>
2022-11-01 21:31:40 -0400

    adjusted search box

--- site/js/search-box.js
+++ site/js/search-box.js
@@ -1,31 +1,25 @@
 // // // add event listener to search box
-// // import data from '../data/list-loader.js';
-// import { showVotersInList } from './voter-list';
-// import { voterList } from './voter-list';
-
-// let voterNameFilter = document.querySelector('#search-box-input');
-// let filteredVoter = data
-// // // function onSearchBoxInput() {
-// // //   showVotersInList();
-// // // }
-
-// // function onSearchBoxInput(data) {
-// //   // showVotersInList();
-// //   let filteredVoter = data;
-// //   const text = voterNameFilter.value;
-// //   filteredVoter = filteredVoter.filter(function(voter) {
-// //         const name = voter['First Name'].toLowerCase();
-// //         const hasText = name.includes(text);
-// //         return hasText;
-// //       });
-// //       return filteredVoter;
-// // }
-
 // function onSearchBoxInput() {
-//   showVotersInList(filteredVoter, voterList);
+//   showVotersInList();
 // }
 
-// // voterNameFilter.addEventListener('input', () => {
-// //   const filteredVoter = onSearchBoxInput();
-// //   showVotersInList(filteredVoter, voterList);
-// // });
+// import { data } from './list-loader.js';
+import { showVotersInList }  from './voter-list.js';
+import { voterList } from './voter-list.js';
+
+let voterNameFilter = document.querySelector('#search-box-input');
+
+function onSearchBoxInput(data) {
+  let filteredVoter = data;
+  const text = voterNameFilter.value;
+  filteredVoter = filteredVoter.filter(function(voter) {
+        const name = voter['First Name'].toLowerCase();
+        const hasText = name.includes(text);
+        return hasText;
+      });
+      return filteredVoter;
+}
+voterNameFilter.addEventListener('input', () => {
+  const filteredVoter = onSearchBoxInput();
+  showVotersInList(filteredVoter, voterList);
+});
