a5e70e13c659114ca48c1297ab7f04f295c5fb3d
henryfeinstein <98499896+henryfeinstein@users.noreply.github.com>
2022-11-03 18:59:42 -0400

    meeting update

--- site/js/dataPull.js
+++ site/js/dataPull.js
@@ -1,14 +1,15 @@
 import { populateVoterMap } from './map.js';
-import { populateVoterList } from './list.js';
+import { populateVoterMenu } from './list.js';
 
 function populateVoterList(listNum, map) {
     fetch('data/voters_lists/' + listNum + '.csv')
     .then(resp => resp.text())
     .then(text => {
+        // TODO: try/catch HTTP error for nonexistent list number
         const data = Papa.parse(text, { header: true });
         console.log(data['data']);
-        populateVoterMap(data, map);
-        populateVoterMenu(data);
+        populateVoterMap(data['data'], map);
+        populateVoterMenu(data['data']);
     });
 }
 
