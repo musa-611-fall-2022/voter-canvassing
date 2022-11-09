ca6f7af9f04fa6ff7bad6cbc3cc78fe0ffe6a701
Mjumbe Poe <mjumbewu@gmail.com>
2022-11-09 13:14:05 -0500

    Use axe tests through puppeteer

--- __tests__/test_a11y.js
+++ __tests__/test_a11y.js
@@ -1,19 +1,11 @@
-/* globals expect, describe, it */
+/* globals beforeAll, expect, describe, it, page */
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { readFile } from 'node:fs/promises';
-import { axe } from 'jest-axe';
-import 'jest-axe/extend-expect';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+beforeAll(async () => {
+  await page.goto('http://localhost:8080/site/');
+});
 
 describe('the index.html page', () => {
   it('should not have any obvious accessibility violations', async () => {
-    const indexPath = `${__dirname}/../site/index.html`;
-    const html = await readFile(indexPath, { encoding: 'utf8' });
-
-    expect(await axe(html)).toHaveNoViolations();
+    await expect(page).toPassAxeTests();
   });
 });
