32d10b11718056309212507e4bc57731e265255a
Morgangriff12 <112583600+Morgangriff12@users.noreply.github.com>
2022-11-12 16:51:05 -0500

    Add comments to explain what I did in the last commit

--- site/js/main.js
+++ site/js/main.js
@@ -8,11 +8,11 @@ let myLocation; // made a global myLocation variable that can be accessed when l
 let voterFileInput = document.querySelector('.input'); // saves input DOM element as variable
 //need to restrict line of input to just 1. currently accepts enter and can fit a paragraph.
 
-let voterFileLoadButton = document.querySelector('#load-voter-list-button'); // saves button DOM element as variable
+let voterFileLoadButton = document.querySelector('#load-voter-list-button'); // saves load voter list button DOM element as variable
 
 let voterListObj = document.querySelector("#voter-list"); // get list we'll put voters in from DOM
 
-let clearInputTextButton = document.querySelector('#clear-text-button')
+let clearInputTextButton = document.querySelector('#clear-text-button') //saves clear input text button as a DOM
 
 function onButtonClicked(evt) { // maybe add functionality that listens for enter button pressed
     listNum = voterFileInput.value;
@@ -40,6 +40,7 @@ function clearInputTextBox () {
     clearInputTextButton.addEventListener('click', onClearInputButtonClicked)
 };
 clearInputTextBox(); 
+//adds an event listener that clears the voter list input text box when the user clicks the Clear Input Text button
 
 locateMe(map, myLocation); // runs function to return your location and mark it on a map. Have to use localhost:8080 for the location to be accessed though.
 
