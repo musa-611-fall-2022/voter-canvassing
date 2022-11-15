793af9013586847c9dd91098a3d2e5482d24b13d
miaomiao612 <dddoctorr612@gmail.com>
2022-11-15 13:16:25 +0800

    update

--- site/js/main.js
+++ site/js/main.js
@@ -2,17 +2,28 @@
  * @Author: miaomiao612 dddoctorr612@gmail.com
  * @Date: 2022-11-10 05:49:08
  * @LastEditors: miaomiao612 dddoctorr612@gmail.com
- * @LastEditTime: 2022-11-14 10:56:31
+ * @LastEditTime: 2022-11-15 11:17:39
  * @FilePath: \voter-canvassing\site\js\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
+// voter canvassing App
+// ============================
+
+// The _main.js_ module defines the primary functionality of the app, and serves
+// as the cross-component coordinator. Additional functionality is found in
+// individual component modules:
+// * [map.js]: for behavior related to the map
+
 import { initMap, showVotersOnMap }  from './map.js';
 
-let voterMap=initMap;
+let voterMap=initMap();
+let votersToShow = document.querySelector('#listNo');
+
 
 //convert csv to json
-function cvstojson (map, neighbor, onFailure){
-    fetch(`./data/voters_lists/${neighbor}.csv`)
+function csvtojson (voterMap, votersToShow, onFailure){
+    fetch(`./data/voters_lists/${votersToShow}.csv`)
     .then(response => {
         if (response.status === 200) {
         const data = response.text();
@@ -28,8 +39,10 @@ function cvstojson (map, neighbor, onFailure){
         let v = result.data.slice(1, result.data.length-1);
         return v;
     })
-    .then(result => showVotersOnMap(map, result));
+    .then(result => showVotersOnMap(result, voterMap));
     }
 
+    csvtojson (voterMap, votersToShow );
+    showVotersOnMap(votersToShow, voterMap);
 window.voterMap=voterMap;
 
--- site/js/map.js
+++ site/js/map.js
@@ -2,12 +2,20 @@
  * @Author: miaomiao612 dddoctorr612@gmail.com
  * @Date: 2022-11-11 03:00:55
  * @LastEditors: miaomiao612 dddoctorr612@gmail.com
- * @LastEditTime: 2022-11-14 11:02:43
+ * @LastEditTime: 2022-11-15 13:15:14
  * @FilePath: \voter-canvassing\site\js\map.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+function onvoterClicked(evt) {
+    console.log(evt);
+    const voter = evt.layer.feature;
+
+    const voterSelectedEvent = new CustomEvent('voter-selected', { detail: { voter } });
+    window.dispatchEvent(voterSelectedEvent);
+}
+
 function initMap() {
-    const map = L.map('map', { maxZoom: 22, preferCanvas: true }).setView([39.95, -75.16], 13);
+    const map = L.map('voterMap', { maxZoom: 22, preferCanvas: true }).setView([39.95, -75.16], 13);
 
     const mapboxAccount = 'mapbox';
     const mapboxStyle = 'light-v10';
@@ -16,8 +24,21 @@ function initMap() {
         maxZoom: 19,
         attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     }).addTo(map);
+    map.treeLayer = L.geoJSON(null, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng),
+        style: {
+        fillColor: '#83bf15',
+        fillOpacity: 0.3,
+        stroke: false,
+        },
+    }).addTo(map);
 
+    map.treeLayer.addEventListener('click', onvoterClicked);
 
+    map.positionLayer = L.geoJSON(null).addTo(map);
+
+    return map;
+}
 
 
 //convert json to a geojson-like feature
@@ -43,16 +64,16 @@ function makevoterFeature(voter)
 }
 
 //Use the function to display the voters' location on the map.
-function showVotersOnMap(votersToShow, Map) {
-    if (Map.voterLayer !== undefined){
-        Map.removeLayer(Map.voterLayer);
+function showVotersOnMap(votersToShow, map) {
+    if (map.voterLayer !== undefined){
+        map.removeLayer(map.voterLayer);
     }
 
     const voterFeatureCollection = {
         "type": "FeatureCollection",
         "features": votersToShow.map(makevoterFeature),
     };
-    map.voterLayer = L.geoJSON(null, {
+    map.voterLayer = L.geoJSON(voterFeatureCollection, {
         pointToLayer: (feature, latlng) => L.circleMarker(latlng),
         style: {
             fillColor: '#83bf15',
@@ -60,8 +81,10 @@ function showVotersOnMap(votersToShow, Map) {
             stroke: false,
         },
         }).addTo(map);
-}
+    }
 
-export{
-    initMap,
-}
+    export{
+        initMap,
+        showVotersOnMap,
+        makevoterFeature,
+};
